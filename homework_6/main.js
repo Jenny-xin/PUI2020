57f0732f5b33a6f3b5653af3204d31dd6dba62db
Jenny-xin <38271148+Jenny-xin@users.noreply.github.com>
2020-10-27 23:05:15 -0400

    files for hw6

---
+++ homework_6/main.js
@@ -0,0 +1,51 @@
+
+var cartArray = []
+
+// class for cinnamon rolls
+class Rolls {
+	constructor(glaze, quantity) {
+		this.glaze = glaze
+		this.quantity = quantity
+	}
+	
+}
+
+var newGlaze = ''
+
+// handle click events for all the glaze buttons
+function glazeClickHandler() {
+	let nodeGlazeButtons = document.querySelectorAll("input.glaze-button")
+	for (let nodeGlazeButton of nodeGlazeButtons) {
+		nodeGlazeButton.onclick = function(e) {
+			let nodeGlazeButtonsActive = document.querySelectorAll("input.buttonSmallWhiteBorderActive")
+			for (let prevActiveButton of nodeGlazeButtonsActive) {
+				prevActiveButton.classList.remove("buttonSmallWhiteBorderActive");
+			}
+
+			let clickedInputBtn = e.target
+			clickedInputBtn.classList.add("buttonSmallWhiteBorderActive");
+			newGlaze = clickedInputBtn.value;
+		}
+	}
+}
+	
+glazeClickHandler()
+
+
+function addToCart() {
+	var quantity = document.getElementById('quantity').value;
+	var quantityNum = parseInt(quantity)
+		for (var i=0; i < quantityNum; i++) {
+			var rolls = new Rolls(newGlaze, quantity)
+			cartArray.push(rolls)
+		}
+		console.log(cartArray)
+	updateCart(cartArray.length)
+}
+
+function updateCart() {
+	var cartCount = document.getElementById('cartCount');
+	if (cartArray.length > 0) {
+	cartCount.innerHTML = 'Cart (' + cartArray.length + ')'
+	}
+}
